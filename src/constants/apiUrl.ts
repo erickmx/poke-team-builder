c274072465b59b97daed368dd043de6bdfa5a19f
Erick Mejia <erickblsnc96@gmail.com>
2023-07-12 23:10:20 -0600

    feat: implemented apollo client

---
+++ src/constants/apiUrl.ts
@@ -0,0 +1,3 @@
+const API_URL = "https://beta.pokeapi.co/graphql/v1beta";
+
+export default API_URL;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ApolloProvider } from "@apollo/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import client from "./api/client";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
